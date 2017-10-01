c72626b5bffee9429c168f3814f717105009dd34
estebanrfp <estebanrfp@gmail.com>
2017-10-01 13:20:03 +0200

    added dist folder

---
+++ dist/firstmodule.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{334:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(335),o=function(e){return e&&e.__esModule?e:{default:e}}(u),r=function(e){document.querySelector(e).innerHTML=(0,o.default)()};t.default=r},335:function(e,t,n){function template(e){var t="";return t+="\n<p>Hello world</p>"}n(124);e.exports=template}});
